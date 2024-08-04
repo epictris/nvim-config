fd8d05ba59daf8137ef31a7135d98ac0bcfded95
Tristan Bray <tristan@uncountable.com>
2024-08-04 11:35:17 +0200

    open oil automatically if nvim is started with a directory as an argument

---
+++ lua/env/init.lua
@@ -0,0 +1,22 @@
+-- Function to check if a path is a directory
+local function is_directory(path)
+  local stat = vim.loop.fs_stat(path)
+  return stat and stat.type == "directory"
+end
+
+-- Get the command line arguments
+local args = vim.fn.argv()
+
+-- Check if there is at least one argument and if it's a directory
+if #args > 0 then
+  if is_directory(args[1]) then
+    print("Neovim started with a directory:", args[1])
+    local oil = require("oil")
+    oil.open()
+    require('oil.util').run_after_load(0, function()
+        if oil.get_cursor_entry() then
+          oil.open_preview()
+        end
+    end)
+  end
+end
