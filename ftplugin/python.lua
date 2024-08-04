960607a223f797fe983483772771cf4281826a40
Tristan Bray <tristan@uncountable.com>
2024-08-04 11:27:58 +0200

    update ruff command syntax

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,7 +1,7 @@
 local format_python = function ()
   local current_file = vim.fn.expand('%:p')
   vim.cmd.w()
-  vim.cmd('!ruff --fix '..current_file)
+  vim.cmd('!ruff check --fix '..current_file)
   vim.cmd('!ruff format '..current_file)
 end
 
