390b3bf31565710e1b2650b444be2ca6a6a7aded
Tristan Bray <tristan@uncountable.com>
2024-10-04 11:17:39 +0200

    add chmod +x keybinding

--- lua/keybinds/init.lua
+++ lua/keybinds/init.lua
@@ -9,5 +9,5 @@ nmap("<leader>tn", "gt", "next tab")
 nmap("<leader>tl", "gT", "last tab")
 nmap("<C-t>", "<C-w>k", "focus above window")
 nmap("<C-n>", "<C-w>j", "focus below window")
-
+nmap("<leader>cx", ":!chmod +x %<CR>", "make file executable")
 
